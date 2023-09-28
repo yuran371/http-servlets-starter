f7a7a89ac9f6be63254485f29b57c27d2baba779
Artem Yuran <yuoran371@gmail.com>
2023-09-28 15:15:50 +0300

    Add UDP's implementation for server client

--- Server/src/com/yuran/http/socket/DatagramRunner.java
+++ Server/src/com/yuran/http/socket/DatagramRunner.java
@@ -1,7 +1,18 @@
 package com.yuran.http.socket;
 
-public class DatagramRunner {
-	public static void main(String[] args) {
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.spi.InetAddressResolver;
 
+public class DatagramRunner {
+	public static void main(String[] args) throws IOException {
+		InetAddress inetAddress = InetAddress.getByName("localhost");
+		try (DatagramSocket datagramSocket = new DatagramSocket()) {
+			byte[] buff = "Hello from UDP client".getBytes();
+			DatagramPacket datagramPacket = new DatagramPacket(buff, buff.length, inetAddress, 7777);
+			datagramSocket.send(datagramPacket);
+		}
 	}
 }
