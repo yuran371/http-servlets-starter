462efa567db774264dde1a04595d9333528cccdf
Artem Yuran <yuoran371@gmail.com>
2023-09-28 14:19:05 +0300

    Add features: infinity cycle

--- Server/src/com/yuran/http/socket/SocketRunner.java
+++ Server/src/com/yuran/http/socket/SocketRunner.java
@@ -7,17 +7,21 @@ import java.io.OutputStream;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class SocketRunner {
 
 	public static void main(String[] args) throws IOException {
 		InetAddress inetAddress = Inet4Address.getByName("localhost");
-		try (Socket socket = new Socket(inetAddress, 7777)) {
+		try (Socket socket = new Socket(inetAddress, 7777);
 			DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
 			DataInputStream inputStream = new DataInputStream(socket.getInputStream());
-			outputStream.writeUTF("Hello world!");
-			System.out.println("Server`s response: " + inputStream.readUTF());
+				Scanner scanner = new Scanner(System.in)) {
+			while (scanner.hasNextLine()) {
+				String request = scanner.nextLine(); 
+				outputStream.writeUTF(request);
+				System.out.println("Server`s response: " + inputStream.readUTF());
+			}
 		}
 	}
-
 }
--- Server/src/com/yuran/http/socket/SocketServerRunner.java
+++ Server/src/com/yuran/http/socket/SocketServerRunner.java
@@ -5,6 +5,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class SocketServerRunner {
 
@@ -12,10 +13,15 @@ public class SocketServerRunner {
 		try (ServerSocket serverSocket = new ServerSocket(7777);
 		Socket socket = serverSocket.accept();
 		DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
-		DataInputStream inputStream = new DataInputStream(socket.getInputStream())) {
-		System.out.println("Client request: " + inputStream.readUTF());
-		outputStream.writeUTF("Hello from server!");
-	}
-	
+		DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+		Scanner scanner = new Scanner(System.in)) {
+		String request = inputStream.readUTF();
+		while (!"stop".equals(request)) {
+			System.out.println("Client request: " + request);
+			String response = scanner.nextLine();
+			outputStream.writeUTF(response);
+			request=inputStream.readUTF();
+			}
+		}
 	}
 }
