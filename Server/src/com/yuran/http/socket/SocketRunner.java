5d1b84a10f9cef88fe4b836bfdba5b2a6da69058
Artem Yuran <yuoran371@gmail.com>
2023-09-29 22:58:36 +0300

    Fixes some field`s format

--- Server/src/com/yuran/http/socket/SocketRunner.java
+++ Server/src/com/yuran/http/socket/SocketRunner.java
@@ -14,11 +14,11 @@ public class SocketRunner {
 	public static void main(String[] args) throws IOException {
 		InetAddress inetAddress = Inet4Address.getByName("localhost");
 		try (Socket socket = new Socket(inetAddress, 7777);
-			DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
-			DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+				DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+				DataInputStream inputStream = new DataInputStream(socket.getInputStream());
 				Scanner scanner = new Scanner(System.in)) {
 			while (scanner.hasNextLine()) {
-				String request = scanner.nextLine(); 
+				String request = scanner.nextLine();
 				outputStream.writeUTF(request);
 				System.out.println("Server`s response: " + inputStream.readUTF());
 			}
--- Server/src/com/yuran/http/socket/SocketServerRunner.java
+++ Server/src/com/yuran/http/socket/SocketServerRunner.java
@@ -11,16 +11,16 @@ public class SocketServerRunner {
 
 	public static void main(String[] args) throws IOException {
 		try (ServerSocket serverSocket = new ServerSocket(7777);
-		Socket socket = serverSocket.accept();
-		DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
-		DataInputStream inputStream = new DataInputStream(socket.getInputStream());
-		Scanner scanner = new Scanner(System.in)) {
-		String request = inputStream.readUTF();
-		while (!"stop".equals(request)) {
-			System.out.println("Client request: " + request);
-			String response = scanner.nextLine();
-			outputStream.writeUTF(response);
-			request=inputStream.readUTF();
+				Socket socket = serverSocket.accept();
+				DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+				DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+				Scanner scanner = new Scanner(System.in)) {
+			String request = inputStream.readUTF();
+			while (!"stop".equals(request)) {
+				System.out.println("Client request: " + request);
+				String response = scanner.nextLine();
+				outputStream.writeUTF(response);
+				request = inputStream.readUTF();
 			}
 		}
 	}
