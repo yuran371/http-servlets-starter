66a816b90a4f5762271357f6dfaa983be6b61fb2
Artem Yuran <yuoran371@gmail.com>
2023-10-03 15:43:07 +0300

    Handle parameters in servlet

--- src/main/java/Server/src/com/yuran/http/servlet/FirstServlet.java
+++ src/main/java/Server/src/com/yuran/http/servlet/FirstServlet.java
@@ -2,6 +2,9 @@ package com.yuran.http.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Enumeration;
+import java.util.Map;
 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.ServletConfig;
@@ -19,9 +22,16 @@ public class FirstServlet extends HttpServlet {
 	
 @Override
 protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	resp.setContentType("text/html");
-	try ( PrintWriter writer = resp.getWriter()) {
-		writer.write("<h1>Hello from First Servlet</h2>");
+	String paramValue = req.getParameter("param");
+	Map<String, String[]> parameterMap = req.getParameterMap();
+	System.out.println();
+	
+	resp.setContentType("text/html; charset=UTF-8");
+	resp.setHeader("token", "12345");
+	
+//	resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+	try (PrintWriter writer = resp.getWriter()) {
+		writer.write("<h1>Привет с певрого сервлета!</h2>");
 	}
 }
 	
